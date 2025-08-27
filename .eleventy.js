88ee8587c97f7525c4981ea50c83baf95c1572b4
Webtactics <paul@webtactics.com.au>
2025-08-28 07:57:37 +1000

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -83,8 +83,7 @@ module.exports = function (eleventyConfig) {
       return content;
     });
 
-  // Metagen plugin
-  eleventyConfig.addPlugin(metagen);
+
 
   // Layout aliases
   eleventyConfig.addLayoutAlias("default", "layouts/grid-default.njk");
