f8c31ff6c4140238cf56f6fdf8685e6adef634a3
Webtactics <paul@webtactics.com.au>
2026-01-12 13:56:40 +1100

    Update helpers.cjs

--- _data/helpers.cjs
+++ _data/helpers.cjs
@@ -2,4 +2,4 @@ var nunjucks = require('nunjucks');
 var env = nunjucks.configure('views');
 
 // Render the template, passing the currentYear in the context object
-env.render('grid-default.njk', { currentYear: new Date().getFullYear() });
+env.render('layouts/grid-default.njk', { currentYear: new Date().getFullYear() });
